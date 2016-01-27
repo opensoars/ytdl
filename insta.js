3da0b813ba60327b94137beaaf084a60ebbf7dfe
opensoars <sam_1700@hotmail.com>
2016-01-27 17:26:28 +0100

    Removed test file

--- insta.js
+++ insta.js
@@ -6,12 +6,12 @@ const prodl = require('./dist/index.js');
 let dls = [];
 let vs = [
   '93zHY-gCvS8',
-  'XcTHAJybjx4',
+/*  'XcTHAJybjx4',
   'nj8t8SCx91g',
   'Me5GvbSHAFk',
   '7FDGY0UCoTc',
   'hyR-m-ZAsCU',
-  'w5E1HIqSXdY'
+  'w5E1HIqSXdY'*/
 ]
 
 vs.forEach(v => {
