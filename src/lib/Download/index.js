6241a9bd291e1ec7899ff07cf3f6a22283f485e4
opensoars <sam_1700@hotmail.com>
2016-01-31 17:50:25 +0100

    start module

--- src/lib/Download/index.js
+++ src/lib/Download/index.js
@@ -125,38 +125,11 @@ let Download = class Download {
   'validateArguments',
   'getUrlFromArguments'
   'validateUrl',
-  'getSourceFromUrl'
+  'getSourceFromUrl',
+  'start'
 ].forEach(Download_module => 
   Download.prototype[Download_module] = require('./lib/' + Download_module);
 );
-
-
-Download.prototype.start = async function start() {
-  try {
-    let t = this;
-
-    let a = await t.validateArguments(t.args);
-    let unvalidated_url = await t.getUrlFromArguments(a);
-    let url = await t.validateUrl(unvalidated_url);
-    let unvalidated_source = await t.getSourceFromUrl(url);
-    let source = await t.getValidatedSource(unvalidated_source);
-    let ytplayer_config = await t.getYtPlayerConfigFromSource(
-      unvalidated_source,
-      t.regexp.ytplayer_config
-    );
-    let fmts = await t.getFmtsFromYtplayerConfig(ytplayer_config);
-    let ranked_fmts = await t.getRankedFmts(fmts);
-    let working_fmt = await t.getWorkingFmt(ranked_fmts, ytplayer_config);
-
-    console.log(working_fmt);
-
-    t.emit('succes', { result: 'result' });
-  }
-  catch (err) {
-    t.emit('error', err);
-  }
-};
-
 
 const WorkingFmtFinder = class WorkingFmtFinder {
   constructor(args) {
